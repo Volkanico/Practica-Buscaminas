c6d37ef18e42ade22528794b2a6ddb7138278941
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2021-03-02 09:54:52 +0100

    Initial commit

---
+++ src/Taula.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Taula {
+
+    private int table[][];
+    private String dificult;
+
+    public Taula (int table, String dificult) {
+
+        this.table[0][0] = table;
+        this.dificult = dificult;
+    }
+
+
+
+    //for (int x=0; x < matriz.length; x++) {
+    //    for (int y=0; y < matriz[x].length; y++) {
+    //        System.out.println (matriz[x][y]);
+    //    }
+    // }
+}
---
+++ src/com/company/Casella.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Casella {
+
+    private boolean mine;
+    private boolean flag;
+    private boolean tap;
+
+    public Casella (boolean mine, boolean flag, boolean tap) {
+        this.mine = mine;
+        this.flag = flag;
+        this.tap = tap;
+    }
+
+
+}
